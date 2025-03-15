498c5c88c7f8c95ccbfc098c25ccde92692fb4ee
Kacper <kacperlachowiczwp.pl@wp.pl>
2025-03-15 01:33:45 +0100

    chore: update coverage configuration to exclude documentation files
    - Added 'docs/**' to the coverage exclusion list in vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: ['**/node_modules/**', '**/test/**', '**/*.d.ts', '**/*.config.ts'],
+      exclude: ['**/node_modules/**', '**/test/**', '**/*.d.ts', '**/*.config.ts', 'docs/**'],
     },
     setupFiles: ['./tests/setup.ts'],
   },
